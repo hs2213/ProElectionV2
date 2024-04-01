f9d2500536711b2e5966396c84fb66e53c2549f6
Hamzah <hsidat@redev.co.uk>
2024-04-01 09:40:59 +0100

    Finished Repo Tests

---
+++ ProElection.Tests.Unit/Repositories/ElectionCodeRepositoryTests.cs
@@ -0,0 +1,94 @@
+﻿namespace ProElection.Tests.Unit.Repositories;
+
+public class ElectionCodeRepositoryTests : DbFaker
+{
+    private IElectionCodeRepository _substituteElectionCodeRepository;
+
+    public ElectionCodeRepositoryTests()
+    {
+        _substituteElectionCodeRepository = Substitute.For<ElectionCodeRepository>(InMemoryDb);
+    }
+    
+    private ElectionCode FakeElectionCode()
+    {
+        return new ElectionCode
+        {
+            Id = Guid.NewGuid(),
+            ElectionId = Guid.NewGuid(),
+            UserId = Guid.NewGuid(),
+            Status = CodeStatus.New
+        };
+    }
+    
+    [Fact]
+    public async Task Create_ShouldAddElectionCodeToDatabase()
+    {
+        // Arrange
+        ElectionCode electionCode = FakeElectionCode();
+        
+        // Act
+        await _substituteElectionCodeRepository.Create(electionCode);
+        ElectionCode? storedElectionCode = await InMemoryDb.ElectionCodes
+            .SingleOrDefaultAsync(code => code.Id == electionCode.Id);
+
+        // Assert
+        Assert.Equivalent(electionCode, storedElectionCode);
+    }
+    
+    [Fact]
+    public async Task GetById_ShouldReturnElectionCode()
+    {
+        // Arrange
+        ElectionCode electionCode = FakeElectionCode();
+        await InMemoryDb.ElectionCodes.AddAsync(electionCode);
+        await InMemoryDb.SaveChangesAsync();
+        
+        // Act
+        ElectionCode? storedElectionCode = await _substituteElectionCodeRepository.GetById(electionCode.Id);
+
+        // Assert
+        Assert.NotNull(storedElectionCode);
+        Assert.Equivalent(electionCode, storedElectionCode);
+    }
+    
+    [Fact]
+    public async Task GetByElectionAndUser_ShouldReturnCorrectElectionCode()
+    {
+        // Arrange
+        ElectionCode electionCode = FakeElectionCode();
+        
+        await InMemoryDb.ElectionCodes.AddAsync(electionCode);
+        await InMemoryDb.SaveChangesAsync();
+        
+        // Act
+        ElectionCode? storedElectionCode = await _substituteElectionCodeRepository
+            .GetByElectionAndUser(electionCode.ElectionId, electionCode.UserId);
+
+        // Assert
+        Assert.NotNull(storedElectionCode);
+        Assert.Equivalent(electionCode, storedElectionCode);
+    }
+    
+    [Fact]
+    public async Task Update_ShouldUpdateElectionCode()
+    {
+        // Arrange
+        ElectionCode electionCode = FakeElectionCode();
+        
+        await InMemoryDb.ElectionCodes.AddAsync(electionCode);
+        await InMemoryDb.SaveChangesAsync();
+        
+        ElectionCode updatedElectionCode = electionCode;
+        updatedElectionCode.Status = CodeStatus.Used;
+        
+        // Act
+        await _substituteElectionCodeRepository.Update(updatedElectionCode);
+        
+        ElectionCode? storedElectionCode = await InMemoryDb.ElectionCodes
+            .SingleOrDefaultAsync(code => code.Id == electionCode.Id);
+
+        // Assert
+        Assert.NotNull(storedElectionCode);
+        Assert.Equivalent(updatedElectionCode, storedElectionCode);
+    }
+}
---
+++ ProElection.Tests.Unit/Repositories/VoteRepositoryTests.cs
@@ -0,0 +1,91 @@
+﻿namespace ProElection.Tests.Unit.Repositories;
+
+public class VoteRepositoryTests : DbFaker
+{
+    private IVoteRepository _substituteVoteRepository;
+
+    public VoteRepositoryTests()
+    {
+        _substituteVoteRepository = Substitute.For<VoteRepository>(InMemoryDb);
+    }
+
+    private Vote FakeVote()
+    {
+        return new Vote
+        {
+            Id = Guid.NewGuid(),
+            UserId = Guid.NewGuid(),
+            CandidateId = Guid.NewGuid(),
+            ElectionId = Guid.NewGuid(),
+            Time = DateTimeOffset.Now,
+        };
+    }
+    
+    [Fact]
+    public async Task Create_Vote_ShouldAddVoteToDb()
+    {
+        // Arrange
+        Vote vote = FakeVote();
+        
+        // Act
+        await _substituteVoteRepository.Create(vote);
+        Vote? voteInDb = await InMemoryDb.Votes.FirstOrDefaultAsync(v => v.Id == vote.Id);
+        
+        // Assert
+        Assert.NotNull(voteInDb);
+        Assert.Equivalent(vote, voteInDb);
+    }
+
+    [Fact]
+    public async Task CheckIfUserVotedInElection_UserVoted_ShouldReturnTrue()
+    {
+        // Arrange
+        Vote vote = FakeVote();
+        await InMemoryDb.Votes.AddAsync(vote);
+        await InMemoryDb.SaveChangesAsync();
+        
+        // Act
+        bool result = await _substituteVoteRepository.CheckIfUserVotedInElection(vote.UserId, vote.ElectionId);
+        
+        // Assert
+        Assert.True(result);
+    }
+    
+    [Fact]
+    public async Task CheckIfUserVotedInElection_UserNotVoted_ShouldReturnFalse()
+    {
+        // Arrange
+        Vote vote = FakeVote();
+        
+        // Act
+        bool result = await _substituteVoteRepository.CheckIfUserVotedInElection(vote.UserId, vote.ElectionId);
+        
+        // Assert
+        Assert.False(result);
+    }
+    
+    [Fact]
+    public async Task GetCandidateVotesByElectionId_ShouldReturnVotesCount()
+    {
+        // Arrange
+        Guid electionId = Guid.NewGuid();
+        Guid candidateId = Guid.NewGuid();
+        
+        Vote vote1 = FakeVote();
+        vote1.ElectionId = electionId;
+        vote1.CandidateId = candidateId;
+        
+        Vote vote2 = FakeVote();
+        vote2.ElectionId = electionId;
+        vote2.CandidateId = candidateId;
+        
+        InMemoryDb.Votes.AddRange([vote1, vote2]);
+        await InMemoryDb.SaveChangesAsync();
+        
+        // Act
+        int result = await _substituteVoteRepository.GetCandidateVotesByElectionId(candidateId, electionId);
+        
+        // Assert
+        Assert.Equal(2, result);
+    }
+}
